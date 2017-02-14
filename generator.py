9a88503111fa2b363bf2e6ffa4122cfa354a2554
janeluck <janeluck158@gmail.com>
2017-02-14 15:47:06 +0800

    init

---
+++ generator.py
@@ -0,0 +1,36 @@
+from pymongo import MongoClient
+from datetime import datetime
+
+client = MongoClient()
+
+client.drop_database('jikexueyuan')
+
+
+db_1 = client['jike']
+col_1 = db_1['spider']
+result = db_1.spider.insert_one(
+    {
+        "address": {
+            "street": "2 Avenue",
+            "zipcode": "10075",
+            "building": "1480",
+            "coord": [-73.9557413, 40.7720266]
+        },
+        "borough": "Manhattan",
+        "cuisine": "Italian",
+        "grades": [
+            {
+                "date": datetime.strptime("2014-10-01", "%Y-%m-%d"),
+                "grade": "A",
+                "score": 11
+            },
+            {
+                "date": datetime.strptime("2014-01-16", "%Y-%m-%d"),
+                "grade": "B",
+                "score": 17
+            }
+        ],
+        "name": "Vella",
+        "restaurant_id": "41704620"
+    }
+)
