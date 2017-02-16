dc75355b1434ce7e1a5cbc88b988c00b7130b31b
jane <jane@janes-MacBook-Pro.local>
2017-02-16 20:56:44 +0800

    regax

---
+++ regax-action.py
@@ -0,0 +1,6 @@
+import re
+a = '我是kingname, 我的微博账号是:kingname, 密码是:12345678, QQ账号是:99999, 密码是:890abcd, 银行卡账号是:000001, 密码是:654321, Github账号是:99999@qq.com, 密码是:7777love8888, 请记住他们。'
+print(re.findall('密码是:(.*)', a))
+print(re.findall('密码是:(.*),', a))
+print(re.findall('密码是:(.*?)', a))
+print(re.findall('密码是:(.*?),', a))
