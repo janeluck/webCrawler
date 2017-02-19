575313968b85f390328887a17ae46252fde0e97d
janeluck <janeluck158@gmail.com>
2017-02-19 10:39:37 +0800

    selenium

---
+++ selenium-example.py
@@ -0,0 +1,3 @@
+from selenium import webdriver
+driver = webdriver.Chrome('/Users/jane/software/chromeDriver/chromedriver')
+driver.get("http://v.youku.com/v_show/id_XMTY2NTk5ODAwMA==.html?from=y1.3-idx-beta-1519-23042.223465.3-3")
