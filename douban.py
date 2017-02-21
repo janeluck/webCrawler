fe6b78aef03e8cd183ccd7da8150652b3a94c438
janeluck <janeluck158@gmail.com>
2017-02-21 09:02:53 +0800

    comment

--- douban.py
+++ douban.py
@@ -2,7 +2,11 @@ import lxml.html
 from multiprocessing.dummy import Pool
 import requests
 import csv
-url = 'https://movie.douban.com/top250?start=0&filter='
+
+
+'''
+多线程抓取豆瓣电影TOP250
+'''
 
 class MovieSpider(object):
     def __init__(self):
@@ -13,7 +17,11 @@ class MovieSpider(object):
         html = self.get_source(page)
         self.get_page(page, html)
 
+
     def get_source(self, page):
+        '''
+        获取网页源信息
+        '''
         url = 'https://movie.douban.com/top250?start={}&filter='.format(page * 25)
         return  requests.get(url).content
 
@@ -21,22 +29,17 @@ class MovieSpider(object):
 
         page_content = []
         selector = lxml.html.fromstring(html)
-
-
         movies = selector.xpath('//div[@class="info"]')
 
-
         for movie in movies:
-
-
-
+            # 组织每一页的电影数据
             page_content.append(
+                # 提取每个电影的标题, 链接, 演职人员, 评分
                 {
                     'title': movie.xpath('div[@class="hd"]/a/span[@class="title"][1]/text()')[0].strip(),
                     'link': movie.xpath('div[@class="hd"]/a[1]/@href')[0].strip(),
                     'actor': movie.xpath('div[@class="bd"]/p[1]/text()')[0].strip(),
                     'rate': movie.xpath('div[@class="bd"]/div[@class="star"]/span[@class="rating_num"][1]/text()')[0].strip()
-
                 }
             )
 
@@ -44,11 +47,13 @@ class MovieSpider(object):
         self.page_dict[page] = page_content
 
     def write_csv(self):
+
+        #按页码排序
         content_list = sorted(self.page_dict.items(), key=lambda x: x[0])
 
         result = []
-        for page, content in content_list:
 
+        for page, content in content_list:
             result.extend(content)
 
         # 生成csv文件
@@ -61,7 +66,8 @@ if __name__ == '__main__':
 
     pool = Pool(10)
     movieSpider = MovieSpider()
-    pool.map(movieSpider.run, [x for x in range(0, 10)]) #使用map的时候,第一个参数是run方法,不加括号
+    #抓取前10页
+    pool.map(movieSpider.run, [x for x in range(0, 10)])
     movieSpider.write_csv()
     print('抓取完成!')
 
