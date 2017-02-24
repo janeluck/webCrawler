50815b3615300a351f39e8eef5cf48903216aded
janeluck <janeluck158@gmail.com>
2017-02-24 16:56:15 +0800

    Guokr

---
+++ LoginGuokr.py
@@ -0,0 +1,52 @@
+import requests
+import lxml.html
+
+class GuokrSpider(object):
+
+    def __init__(self):
+        self.s = requests.Session()
+        form = self.get_form()
+        self.login(form)
+
+
+
+
+    def get_form(self):
+
+        # 获取验证码相关信息: token和图片请求地址url
+        html = lxml.html.fromstring(self.s.get('https://account.guokr.com/sign_in/').content)
+
+        captcha_rand = html.xpath('//input[@id="captchaRand"][1]/@value')[0]
+
+
+        # 下载验证码图片
+        with open('captcha.png', 'wb') as f:
+            f.write(self.s.get('https://account.guokr.com/captcha/' + captcha_rand).content)
+
+        # 返回登录所需的表单信息
+        return {
+            'username': 'greensouth@foxmail.com',
+            'password': 'iamtheone847',
+            'csrf_token': html.xpath('//input[@id="csrf_token"][1]/@value')[0],
+            'captcha_rand': captcha_rand,
+            'captcha': input('请查看captcha.png, 输入验证码:\n'),
+            'permanent': 'y'
+
+        }
+
+
+    def login(self, form):
+
+
+        # 请求登录接口
+        self.s.post('https://account.guokr.com/sign_in/', data=form)
+        print(self.s.post('https://account.guokr.com/sign_in/', data=form).content.decode())
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    GuokrSpider()
